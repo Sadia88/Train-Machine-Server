48f83bf12ccd83375a64331a7e9d75f0802662a2
Sadia88 <sadiasharmin460280@gmail.com>
2022-10-27 01:23:34 +0600

    course_fee is added

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const express = require('express')
 var cors = require('cors')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 
 
 app.use(cors())
+const categories=require('./Data/categories.json')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Server is running!')
 })
-https://runkit.com/embed/save
-app.listen(port, () => {
+
+app.get('/courses', (req, res) => {
+    console.log(req.params.id)
+  res.send(categories)
+})
+
+
+
+app.get('/courses/course/:id', (req, res) => {
+  const _id=req.params.id
+
+     const category_news=categories.find(n=>n.id==_id)
+     res.send(category_news)
+ console.log(category_news)
+  
+ })
+
+
+
+
+
+
+
+ 
+
+ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
